486ac88f8934c757771b427d8825fc1f209fb070
Seamanddup <tanghaiyangddup@163.com>
2017-12-08 09:10:41 +0800

    add

---
+++ PyTest001.py
@@ -0,0 +1,10 @@
+# -*- coding: cp936 -*-
+print 'x'
+
+print 'input ur name 如果输入的是非数字，需要用引号'
+print("python还有一个输入的方法：raw_input()，它把所有的输入都直接当作一串字符，于是就可以不用加引号")
+print("py3里的input()相当于py2的raw_input()，得到的都是字符串，而非数值！")
+input()
+print 'ur name is '
+
+print ('xxxx')
---
+++ PyTest002_bianliang.py
@@ -0,0 +1,10 @@
+
+# 变量
+# 字符串 - 表示一串字符，需要用''或""引起来
+# 整数
+# 浮点数 - 就是小数
+# bool（布尔） - 这个比较特殊，是用来表示逻辑“是”“非”的一种类型，它只有两个值，True和False。（注意这里没有引号，有了引号就变成字符串了）
+print "Who do you think I am?"
+you = input()
+print "Oh, yes! I am a"
+print you
